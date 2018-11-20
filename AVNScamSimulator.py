a24d90448e6bb72f120bbc7395aa0456ca4c0423
James Smith <smi.james.th@gmail.com>
2018-11-20 14:51:14 +0200

    Rearranged declaration of sensors a bit, added pointing model sensors, set random value for the first one so that it can be played around with.

--- AVNScamSimulator.py
+++ AVNScamSimulator.py
@@ -29,25 +29,85 @@ class ScamSimulator(DeviceServer):
 
     def setup_sensors(self):
         # Position sensors
-        self._SCM_request_azim = Sensor.float("SCM.request-azim",
-                                              "Sky-space requested azimuth position.", "Degrees CW of north")
-        self._SCM_request_elev = Sensor.float("SCM.request-elev",
-                                              "Sky-space requested elevation position.", "Degrees CW of north")
-        self._SCM_desired_azim = Sensor.float("SCM.desired-azim",
-                                              "Sky-space desired azimuth position.", "Degrees CW of north")
-        self._SCM_desired_elev = Sensor.float("SCM.desired-elev",
-                                              "Sky-space desired elevation position.", "Degrees CW of north")
-        self._SCM_actual_azim = Sensor.float("SCM.actual-azim",
-                                             "Sky-space actual azimuth position.", "Degrees CW of north")
-        self._SCM_actual_elev = Sensor.float("SCM.actual-elev",
-                                             "Sky-space actual elevation position.", "Degrees CW of north")
-
+        self._SCM_request_azim = Sensor.float("SCM.request-azim", "Sky-space requested azimuth position.", "Degrees CW of north")
         self.add_sensor(self._SCM_request_azim)
+
+        self._SCM_request_elev = Sensor.float("SCM.request-elev", "Sky-space requested elevation position.", "Degrees CW of north")
         self.add_sensor(self._SCM_request_elev)
+
+        self._SCM_desired_azim = Sensor.float("SCM.desired-azim", "Sky-space desired azimuth position.", "Degrees CW of north")
         self.add_sensor(self._SCM_desired_azim)
+
+        self._SCM_desired_elev = Sensor.float("SCM.desired-elev", "Sky-space desired elevation position.", "Degrees CW of north")
         self.add_sensor(self._SCM_desired_elev)
+
+        self._SCM_actual_azim = Sensor.float("SCM.actual-azim", "Sky-space actual azimuth position.", "Degrees CW of north")
         self.add_sensor(self._SCM_actual_azim)
+
+        self._SCM_actual_elev = Sensor.float("SCM.actual-elev", "Sky-space actual elevation position.", "Degrees CW of north")
         self.add_sensor(self._SCM_actual_elev)
+
+        # Pointing model
+        self._SCM_pmodel1 = Sensor.float("SCM.pmodel1", "Pointing model parameter 1")
+        self.add_sensor(self._SCM_pmodel1)
+        self._SCM_pmodel2 = Sensor.float("SCM.pmodel2", "Pointing model parameter 2")
+        self.add_sensor(self._SCM_pmodel2)
+        self._SCM_pmodel3 = Sensor.float("SCM.pmodel3", "Pointing model parameter 3")
+        self.add_sensor(self._SCM_pmodel3)
+        self._SCM_pmodel4 = Sensor.float("SCM.pmodel4", "Pointing model parameter 4")
+        self.add_sensor(self._SCM_pmodel4)
+        self._SCM_pmodel5 = Sensor.float("SCM.pmodel5", "Pointing model parameter 5")
+        self.add_sensor(self._SCM_pmodel5)
+        self._SCM_pmodel6 = Sensor.float("SCM.pmodel6", "Pointing model parameter 6")
+        self.add_sensor(self._SCM_pmodel6)
+        self._SCM_pmodel7 = Sensor.float("SCM.pmodel7", "Pointing model parameter 7")
+        self.add_sensor(self._SCM_pmodel7)
+        self._SCM_pmodel8 = Sensor.float("SCM.pmodel8", "Pointing model parameter 8")
+        self.add_sensor(self._SCM_pmodel8)
+        self._SCM_pmodel9 = Sensor.float("SCM.pmodel9", "Pointing model parameter 9")
+        self.add_sensor(self._SCM_pmodel9)
+        self._SCM_pmodel10 = Sensor.float("SCM.pmodel10", "Pointing model parameter 10")
+        self.add_sensor(self._SCM_pmodel10)
+        self._SCM_pmodel11 = Sensor.float("SCM.pmodel11", "Pointing model parameter 11")
+        self.add_sensor(self._SCM_pmodel11)
+        self._SCM_pmodel12 = Sensor.float("SCM.pmodel12", "Pointing model parameter 12")
+        self.add_sensor(self._SCM_pmodel12)
+        self._SCM_pmodel13 = Sensor.float("SCM.pmodel13", "Pointing model parameter 13")
+        self.add_sensor(self._SCM_pmodel13)
+        self._SCM_pmodel14= Sensor.float("SCM.pmodel14", "Pointing model parameter 14")
+        self.add_sensor(self._SCM_pmodel14)
+        self._SCM_pmodel15= Sensor.float("SCM.pmodel15", "Pointing model parameter 15")
+        self.add_sensor(self._SCM_pmodel15)
+        self._SCM_pmodel16 = Sensor.float("SCM.pmodel16", "Pointing model parameter 16")
+        self.add_sensor(self._SCM_pmodel16)
+        self._SCM_pmodel17 = Sensor.float("SCM.pmodel17", "Pointing model parameter 17")
+        self.add_sensor(self._SCM_pmodel17)
+        self._SCM_pmodel18 = Sensor.float("SCM.pmodel18", "Pointing model parameter 18")
+        self.add_sensor(self._SCM_pmodel18)
+        self._SCM_pmodel19 = Sensor.float("SCM.pmodel19", "Pointing model parameter 19")
+        self.add_sensor(self._SCM_pmodel19)
+        self._SCM_pmodel20 = Sensor.float("SCM.pmodel20", "Pointing model parameter 20")
+        self.add_sensor(self._SCM_pmodel20)
+        self._SCM_pmodel21 = Sensor.float("SCM.pmodel21", "Pointing model parameter 21")
+        self.add_sensor(self._SCM_pmodel21)
+        self._SCM_pmodel22 = Sensor.float("SCM.pmodel22", "Pointing model parameter 22")
+        self.add_sensor(self._SCM_pmodel22)
+        self._SCM_pmodel23 = Sensor.float("SCM.pmodel23", "Pointing model parameter 23")
+        self.add_sensor(self._SCM_pmodel23)
+        self._SCM_pmodel24 = Sensor.float("SCM.pmodel24", "Pointing model parameter 24")
+        self.add_sensor(self._SCM_pmodel24)
+        self._SCM_pmodel25 = Sensor.float("SCM.pmodel25", "Pointing model parameter 25")
+        self.add_sensor(self._SCM_pmodel25)
+        self._SCM_pmodel26 = Sensor.float("SCM.pmodel26", "Pointing model parameter 26")
+        self.add_sensor(self._SCM_pmodel26)
+        self._SCM_pmodel27 = Sensor.float("SCM.pmodel27", "Pointing model parameter 27")
+        self.add_sensor(self._SCM_pmodel27)
+        self._SCM_pmodel28 = Sensor.float("SCM.pmodel28", "Pointing model parameter 28")
+        self.add_sensor(self._SCM_pmodel28)
+        self._SCM_pmodel29 = Sensor.float("SCM.pmodel29", "Pointing model parameter 29")
+        self.add_sensor(self._SCM_pmodel29)
+        self._SCM_pmodel30 = Sensor.float("SCM.pmodel30", "Pointing model parameter 30")
+        self.add_sensor(self._SCM_pmodel30)
 
         self.animation_thread = threading.Thread(target=self.sensor_value_thread_function)
         self.animation_thread.start()
@@ -59,6 +119,9 @@ class ScamSimulator(DeviceServer):
         antenna = katpoint.Antenna(antenna_str)
         my_target = AVNTarget("name1 | *name 2, radec, 12:34:56.7, -04:34:34.2, (1000.0 2000.0 1.0)",
                               antenna=antenna)
+
+        self._SCM_pmodel1.set_value(random.random())
+        
 
         import IPython; IPython.embed()
 
